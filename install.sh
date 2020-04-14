d7ed6699bf4e9421228f6b195d8659d6585e9847
securibee <securibee@gmail.com>
2020-04-14 15:09:11 -0400

    fix: add global hooks path

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 DIR=$HOME/.git/hooks
 
+echo "Setting global hooks path"
+git config --global core.hooksPath $DIR
+
 mkdir -p $DIR 
 cp post-commit $DIR/post-commit
 [ -f $DIR/post-commit ] && echo "Copied post-commit" || echo "Error"
